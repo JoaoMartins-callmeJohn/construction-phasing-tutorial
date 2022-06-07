a3448fe0d1f65a65cf699716120f213951facec4
JoaoMartins-Forge <joao.martins@autodesk.com>
2022-06-07 18:32:55 -0300

    steptwo done

--- wwwroot/extensions/PhasingPanel.js
+++ wwwroot/extensions/PhasingPanel.js
@@ -25,6 +25,28 @@ export class PhasingPanel extends Autodesk.Viewing.UI.DockingPanel {
     this.div = document.createElement('div');
     this.container.appendChild(this.div);
 
+    //Here we create a switch to control vision of the schedule based on the GANTT chart
+    this.checkbox = document.createElement('input');
+    this.checkbox.type = 'checkbox';
+    this.checkbox.id = 'colormodel';
+    this.checkbox.style.width = (this.options.checkboxWidth || 30) + 'px';
+    this.checkbox.style.height = (this.options.checkboxHeight || 28) + 'px';
+    this.checkbox.style.margin = '0 0 0 ' + (this.options.margin || 5) + 'px';
+    this.checkbox.style.verticalAlign = (this.options.verticalAlign || 'middle');
+    this.checkbox.style.backgroundColor = (this.options.backgroundColor || 'white');
+    this.checkbox.style.borderRadius = (this.options.borderRadius || 8) + 'px';
+    this.checkbox.style.borderStyle = (this.options.borderStyle || 'groove');
+
+    this.checkbox.onchange = this.handleColors.bind(this);
+    this.div.appendChild(this.checkbox);
+
+    this.label = document.createElement('label');
+    this.label.for = 'colormodel';
+    this.label.innerHTML = 'Show Phases';
+    this.label.style.fontSize = (this.options.fontSize || 18) + 'px';
+    this.label.style.verticalAlign = (this.options.verticalAlign || 'middle');
+    this.div.appendChild(this.label);
+
     //Here we add the svg for the GANTT chart
     this.content = document.createElement('div');
     this.content.style.backgroundColor = (this.options.backgroundColor || 'white');
@@ -47,23 +69,49 @@ export class PhasingPanel extends Autodesk.Viewing.UI.DockingPanel {
     });
     if (phasing_config.tasks.length > 0) {
       this.gantt = this.createGanttChart();
-      this.handleColors.call(this);
-      this.changeViewMode.call(this);
     }
   }
 
-  async importCSV() {
-    await this.inputCSV();
-    this.gantt = this.createGanttChart();
-    this.handleColors.call(this);
-  }
+  checkTaskStatus(task) {
+    let currentDate = new Date();
 
-  createGanttChart() {
-    document.getElementById('phasing-container').innerHTML = `<svg id="phasing-container"></svg>`;
+    let taskStart = new Date(task._start);
+    let taskEnd = new Date(task._end);
 
-    let newGantt = new Gantt("#phasing-container", phasing_config.tasks);
+    let shouldHaveStarted = currentDate > taskStart;
+    let shouldHaveEnded = currentDate > taskEnd;
 
-    return newGantt;
+    let taskProgress = parseInt(task.progress, 10);
+
+    //We need to map finished, in progress, late, not yet started or advanced
+    //finished should have started and ended and actually ended (progress 100%)
+    //in progress should have started, not ended and progress should be greater than 0
+    //late should have started and ended but progress is less than 100, or should have started not ended and progress is 0
+    //not yet started should not have started nor ended and progress is 0
+    //advanced should not have started and have progress greater than 0 or should not have ended and progress is 100
+
+    if (shouldHaveStarted && shouldHaveEnded && taskProgress === 100)
+      return 'finished';
+
+    else if (shouldHaveStarted && !shouldHaveEnded) {
+      switch (taskProgress) {
+        case 100:
+          return 'advanced';
+        case 0:
+          return 'late';
+        default:
+          return 'inProgress';
+      }
+    }
+
+    else if (shouldHaveStarted && shouldHaveEnded && taskProgress < 100)
+      return 'late';
+
+    else if (!shouldHaveStarted && !shouldHaveEnded && taskProgress === 0)
+      return 'notYetStarted';
+
+    else if (!shouldHaveStarted && taskProgress > 0)
+      return 'advanced';
   }
 
   updateObjects(result) {
@@ -74,6 +122,77 @@ export class PhasingPanel extends Autodesk.Viewing.UI.DockingPanel {
 
       phasing_config.objects[currentTaskId].push(result.dbId);
     }
+  }
+
+  async importCSV() {
+    await this.inputCSV();
+    this.gantt = this.createGanttChart();
+  }
+
+  createGanttChart() {
+    document.getElementById('phasing-container').innerHTML = `<svg id="phasing-container"></svg>`;
+
+    let newGantt = new Gantt("#phasing-container", phasing_config.tasks, {
+      on_click: this.barCLickEvent.bind(this),
+      on_progress_change: this.handleColors.bind(this),
+      on_date_change: this.handleColors.bind(this)
+    });
+
+    return newGantt;
+  }
+
+  handleColors() {
+    this.handleElementsColor.call(this);
+    this.handleBarsColor.call(this);
+  }
+
+  handleElementsColor() {
+    const overrideCheckbox = document.getElementById('colormodel');
+    if (overrideCheckbox.checked) {
+      let tasksNStatusArray = this.gantt.tasks.map(this.checkTaskStatus);
+      let mappeddbIds = [];
+      for (let index = 0; index < this.gantt.tasks.length; index++) {
+        const currentTaskId = this.gantt.tasks[index].id;
+        const currentdbIds = phasing_config.objects[currentTaskId];
+        const colorVector4 = this.fromRGB2Color(phasing_config.statusColors[tasksNStatusArray[index]]);
+        currentdbIds.forEach(dbId => {
+          if (colorVector4) {
+            this.extension.viewer.setThemingColor(dbId, colorVector4)
+          }
+          else {
+            this.extension.viewer.hide(dbId);
+          }
+        });
+        mappeddbIds.push(...currentdbIds);
+      }
+      this.extension.viewer.isolate(mappeddbIds);
+    }
+    else {
+      this.extension.viewer.clearThemingColors();
+      this.extension.viewer.showAll();
+    }
+  }
+
+  handleBarsColor() {
+    this.gantt.bars.map(bar => {
+      let tasksStatus = this.checkTaskStatus(bar.task);
+      let barColor = phasing_config.statusColors[tasksStatus];
+      bar.$bar.style = `fill: rgb(${barColor})`;
+    })
+  }
+
+  fromRGB2Color(rgbString) {
+    if (rgbString) {
+      let colorsInt = rgbString.replaceAll(' ', '').split(',').map(colorString => parseInt(colorString, 10));
+      return new THREE.Vector4(colorsInt[0] / 255, colorsInt[1] / 255, colorsInt[2] / 255, 0.5);
+    }
+    else {
+      return null;
+    }
+  }
+
+  barCLickEvent(task) {
+    this.extension.viewer.isolate(phasing_config.objects[task.id]);
   }
 
   updateTasks() {
@@ -125,11 +244,10 @@ export class PhasingPanel extends Autodesk.Viewing.UI.DockingPanel {
       newObject.dependencies = parameters[inputHeaders.findIndex(h => h === phasing_config.requiredProps.dependencies)];
       newObject.dependencies.replaceAll('-', ',');
     });
-    this.addPropToMask(parameters[inputHeaders.findIndex(h => h === phasing_config.propFilter)], newObject.id);
+    this.addPropToMap(parameters[inputHeaders.findIndex(h => h === phasing_config.propFilter)], newObject.id);
     return newObject;
   }
-
-  addPropToMask(filterValue, taskId) {
+  addPropToMap(filterValue, taskId) {
     phasing_config.mapTaksNProps[filterValue] = taskId;
   }
 
--- wwwroot/extensions/config.js
+++ wwwroot/extensions/config.js
@@ -1,7 +1,5 @@
 export const phasing_config = {
   "tasks": [],
-  "objects": {},
-  "propFilter": "Type Name",
   "requiredProps": {
     "id": "ID",
     "taskName": "NAME",
@@ -10,5 +8,14 @@ export const phasing_config = {
     "taskProgress": "PROGRESS",
     "dependencies": "DEPENDENCIES"
   },
-  "mapTaksNProps": {}
+  "objects": {},
+  "propFilter": "Type Name",
+  "mapTaksNProps": {},
+  "statusColors": {
+    "finished": "31,246,14",
+    "inProgress": "235,246,14",
+    "late": "246,55,14",
+    "notYetStarted": "",
+    "advanced": "14,28,246"
+  }
 }
